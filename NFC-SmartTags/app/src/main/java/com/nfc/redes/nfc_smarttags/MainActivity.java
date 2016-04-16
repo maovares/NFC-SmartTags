743283ad58d2d012720676f3c94cf63e7e2e0fc0
maovares <mrodriguezovares@gmail.com>
2016-04-16 10:46:28 -0600

    Añadido proyecto inicial

---
+++ NFC-SmartTags/app/src/main/java/com/nfc/redes/nfc_smarttags/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nfc.redes.nfc_smarttags;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
